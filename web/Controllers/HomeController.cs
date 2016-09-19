0058ccc2f4fad0b9c1e01d94e6733105fa4687e0
Guilherme Suzuki <linkedin@guilhermesuzuki.com>
2016-09-19 18:51:32 -0400

    initial commit

---
+++ web/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Gma.QrCodeNet.Encoding;
+using Gma.QrCodeNet.Encoding.Windows.Render;
+using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(new string[] { "www.uol.com.br", "http://cp24.com", "http://guilhermesuzuki.com" });
+        }
+
+        [HttpGet]
+        public FileResult QrCode(string url)
+        {
+            if (string.IsNullOrWhiteSpace(url) == false)
+            {
+                var encoder = new QrEncoder(ErrorCorrectionLevel.L);
+                var qr = new QrCode();
+                if (encoder.TryEncode(url, out qr) == true)
+                {
+                    using (var ms = new MemoryStream { })
+                    {
+                        var render = new GraphicsRenderer(new FixedModuleSize(10, QuietZoneModules.Two));
+                        render.WriteToStream(qr.Matrix, ImageFormat.Png, ms);
+                        return File(ms.GetBuffer(), "image/png");
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
